d93b37ed51ec86b2f9e641e52e9fe6779cdb950d
Tomas Janousek <tomi@nomi.cz>
2024-05-19 02:38:51 +0100

    Upload artifacts for debugging
    And also to enable easier development of custom jq filters (to be
    introduced later).

--- run.sh
+++ run.sh
@@ -4,28 +4,32 @@ set -eu -o pipefail
 
 : "${LINKCHECKERRC:?}" "${RETRIES:?}"
 
-echo "::group::Preparing config"
-cfg=$(mktemp)
-echo "$LINKCHECKERRC" > "$cfg"
+echo "::group::Setup temporary directory"
+tmpdir=$(mktemp -d)
+echo "dir=$tmpdir" >> "$GITHUB_OUTPUT"
+cd "$tmpdir"
 echo "::endgroup::"
 
-echo "::group::Finding linkchecker's create.sql"
+echo "$LINKCHECKERRC" > linkcheckerrc
+
+echo "::group::Find linkchecker's create.sql"
 pip_show=$(pipx runpip LinkChecker show -f LinkChecker)
 create_sql_loc1=$(grep -P -o '^Location:\s+\K.*' <<<"$pip_show")
 create_sql_loc2=$(grep -P -o '^\s+\K.*/create\.sql$' <<<"$pip_show")
 [[ "$create_sql_loc1" && "$create_sql_loc2" ]]
 create_sql_loc="$create_sql_loc1/$create_sql_loc2"
 [[ -e "$create_sql_loc" ]]
+sed -e '/^drop table /Id' "$create_sql_loc" > create.sql
 echo "::endgroup::"
 
 db=
 for ((try=1; try <= RETRIES; ++try)); do
   echo "::group::Attempt $try"
 
-  sql=$(mktemp)
-  db=$(mktemp)
-  sed -e '/^drop table /Id' "$create_sql_loc" | sqlite3 "$db"
-  linkchecker --config="$cfg" --file-output=sql/"$sql" "$@" || [[ $? == 1 ]]
+  sql=try"$try".sql
+  db=try"$try".db
+  sqlite3 "$db" < create.sql
+  linkchecker --config=linkcheckerrc --file-output=sql/"$sql" "$@" || [[ $? == 1 ]]
   sqlite3 "$db" < "$sql"
 
   errors=$(sqlite3 "$db" "SELECT COUNT(*) FROM linksdb WHERE NOT valid")
@@ -38,6 +42,8 @@ for ((try=1; try <= RETRIES; ++try)); do
   echo "::endgroup::"
 done
 
-sqlite3 "$db" '.mode json' 'SELECT * FROM linksdb' | "$GITHUB_ACTION_PATH"/output.jq
+[[ $db ]]
+sqlite3 "$db" '.mode json' 'SELECT * FROM linksdb' > result.json
+"$GITHUB_ACTION_PATH"/output.jq result.json
 
 [[ $errors == 0 ]]
