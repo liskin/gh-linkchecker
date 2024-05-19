6f9b9238992a103faefc382d5f915dc57e5d1b4e
Tomas Janousek <tomi@nomi.cz>
2024-05-19 12:09:11 +0100

    Retab run.sh

--- run.sh
+++ run.sh
@@ -25,35 +25,35 @@ echo "::endgroup::"
 
 errors=1
 for ((try=1; try <= RETRIES; ++try)); do
-  echo "::group::Attempt $try"
+	echo "::group::Attempt $try"
 
-  sql=try"$try".sql
-  json=try"$try".json
+	sql=try"$try".sql
+	json=try"$try".json
 
-  # run linkchecker, continue if its return value is 0 (success) or 1 (warnings/errors)
-  linkchecker --config=linkcheckerrc --file-output=sql/"$sql" "$@" || [[ $? == 1 ]]
+	# run linkchecker, continue if its return value is 0 (success) or 1 (warnings/errors)
+	linkchecker --config=linkcheckerrc --file-output=sql/"$sql" "$@" || [[ $? == 1 ]]
 
-  # convert output to json
-  sqlite3 "" \
-    ".read create.sql" \
-    ".read $sql" \
-    ".mode json" \
-    "SELECT * FROM linksdb" \
-    > "$json"
-  [[ -s "$json" ]] || echo "[]" > "$json"
+	# convert output to json
+	sqlite3 "" \
+		".read create.sql" \
+		".read $sql" \
+		".mode json" \
+		"SELECT * FROM linksdb" \
+		> "$json"
+	[[ -s "$json" ]] || echo "[]" > "$json"
 
-  # invoke custom filter, if any
-  jq -c -L . -f filter.jq "$json" > result.json
+	# invoke custom filter, if any
+	jq -c -L . -f filter.jq "$json" > result.json
 
-  # retry if there are any errors
-  if jq -e 'all(.valid | . != 0)' result.json; then
-    errors=0
-    break
-  else
-    sleep 30
-  fi
+	# retry if there are any errors
+	if jq -e 'all(.valid | . != 0)' result.json; then
+		errors=0
+		break
+	else
+		sleep 30
+	fi
 
-  echo "::endgroup::"
+	echo "::endgroup::"
 done
 
 echo "::group::Results"
